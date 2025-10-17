8ec67c8c92e3f190b93b5f63f5fcf14ef7c7f466
Gisela-Camacho-H <gis25cam@gmail.com>
2025-10-17 15:28:33 -0600

    first commit: Adding Xcode Project

---
+++ MoodMate/MoodMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoodMate
+//
+//  Created by Gis Cam on 17/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoodMate/MoodMate/MoodMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMateApp.swift
+//  MoodMate
+//
+//  Created by Gis Cam on 17/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
