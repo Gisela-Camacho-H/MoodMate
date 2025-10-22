6751d12b6f0c3e338d087729654bbe5b94479a89
Gisela-Camacho-H <gis25cam@gmail.com>
2025-10-22 11:12:47 -0600

    Adding tabSelection and updation tabView

---
+++ MoodMate/MoodMate/Common/TabSelection.swift
@@ -0,0 +1,21 @@
+//
+//  TabSelection.swift
+//  MoodMate
+//
+//  Created by Gis Cam on 22/10/25.
+//
+
+import SwiftUI
+import Combine
+
+enum TabSelection: String {
+    case home = "Home"
+    case mood = "Mood"
+    case emotions = "Emotions"
+    case track = "Track"
+    case profile = "Profile"
+}
+
+class TabMaganer: ObservableObject {
+    @Published var selectionTab: TabSelection = .home
+}
--- MoodMate/MoodMate/TabView/MainTabView.swift
+++ MoodMate/MoodMate/TabView/MainTabView.swift
@@ -8,30 +8,37 @@
 import SwiftUI
 
 struct MainTabView: View {
+    @StateObject var tabManager = TabMaganer()
     var body: some View {
-        TabView {
+        TabView(selection: $tabManager.selectionTab) {
             HomeView()
                 .tabItem {
                     Label("Home", systemImage: "house")
                 }
+                .tag(TabSelection.home)
             MoodView()
                 .tabItem {
                     Label("Mood", systemImage: "apple.meditate")
                 }
+                .tag(TabSelection.mood)
             EmotionsView()
                 .tabItem {
                     Label("Emotions", systemImage: "face.smiling")
                 }
+                .tag(TabSelection.emotions)
             TrackView()
                 .tabItem {
                     Label("Track", systemImage: "list.bullet")
                 }
+                .tag(TabSelection.track)
             ProfileView()
                 .tabItem {
                     Label("Profile", systemImage: "person")
                 }
+                .tag(TabSelection.profile)
         }
         .accentColor(Color("BlueMood"))
+        .navigationBarHidden(true)
     }
 }
 
